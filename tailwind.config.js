67c088a0f14480ed174e11f85b8cb6b04726a008
Julien <julien.morel76350@gmail.com>
2023-11-21 15:34:00 +0100

    🚀 End

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
   theme: {
-    extend: {},
+    colors: {
+      primary: "#f7f7f7",
+      light: "#fff",
+      dark: "#000",
+      secondary: "#828282",
+    },
+    borderRadius: {
+      "2xl": "24px",
+      full: "9999px",
+    },
+    fontFamily: {
+      popoins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        "banner-img": "url('/src/assets/images/img.jpeg')",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
